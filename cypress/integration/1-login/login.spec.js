f1a3dfb1e8a46dd5cc7c6a7094f05a0c28980d10
hirokinishiura <h_nishiura@bs-ja.co.jp>
2022-04-26 17:52:46 +0900

    Add login test

--- cypress/integration/1-login/login.spec.js
+++ cypress/integration/1-login/login.spec.js
@@ -5,19 +5,24 @@ describe('ログイン画面', () => {
     cy.visit('http://localhost:3000')
   })
 
-  // it('ログイン出来るかテスト', () => {
-  //   cy.get('#mail-address').type('nishiurahiroki@gmail.com')
-  //   cy.get('#password').type('********')
-  //   cy.get('button').contains('ログイン').click()
+  it('ログイン出来るか確認するテスト', () => {
+    cy.get('#mail-address').type('nishiura.t2yjam@gmail.com')
+    cy.get('#password').type('cypress-test')
+    cy.get('button').contains('ログイン').click()
 
-  //   cy.get('h6').contains('出費入力')
-  // })
+    cy.get('h6').contains('出費入力')
+  })
+
+  it('ログイン画面が正しく表示されているか確認するテスト', () => {
+    cy.get('#mail-address')
+    cy.get('#password')
+    cy.get('button').contains('ログイン')
+  })
 
   it('ログイン失敗時にエラーメッセージが表示されるか確認するテスト', () => {
     cy.get('#mail-address').type('nishiurahiroki@gmail.com')
     cy.get('#password').type('********')
     cy.get('button').contains('ログイン').click()
-
     cy.get('#successMessage').contains('ログインに失敗しました')
   })
 })
